02f764dd9ca7a4f42882d4be471839081af1a3e9
TiagoGodinho24 <tfgodinho@gmail.com>
2023-11-22 15:42:15 +0000

    erro createconfiguration

---
+++ TFSSincronizador/Models/MappingType.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TFSSincronizador.Models
+{
+
+    public class MappingType
+    {
+        [Key]
+        public int MappingTypeId { get; set; }
+
+        [ForeignKey("Profile")]
+        public int MappingTypeProfile { get; set; }
+
+        [ForeignKey("TFSType")]
+        public int TFSTypeId { get; set; }
+        public TFSType TFSType { get; set; }
+
+        [ForeignKey("JiraType")]
+        public int JiraTypeId { get; set; }
+        public JiraType JiraType { get; set; }
+
+        public Profile Profile { get; set; }
+    }
+}
